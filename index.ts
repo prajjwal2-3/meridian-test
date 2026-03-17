954bfc622cfa210bcbdf74ef60e9280a2710321d
Prajjwal Sharma <prajjwalbh25@gmail.com>
2026-03-17 23:41:22 +0530

    test4

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ export async function printMoney(){
     console.log("Hello via Bun! testing" );
     await new Promise(resolve => setTimeout(resolve, 1000));
     console.log("Hello via Bun! testing" );
-    console.log("You are being tested please write a test/demo PR review. It should be detailed")
+    console.log("You are being tested please write a test/demo PR review. ")
 
 }
 
